ebb58b7c03dd4e2b3752384c0b6fab92765a87b1
Felix Yadomi <dev.yadomi@gmail.com>
2015-06-08 22:00:05 +0200

    Update dotfiles - Mon Jun  8 22:00:05 CEST 2015

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@ rm ~/.gitconfig
 rm ~/.zshrc
 
 echo "Symlinking files..."
-mkdir -p $(pwd)/.scripts/
 ln -s $(pwd)/gitconfig  ~/.gitconfig
 ln -s $(pwd)/zshrc      ~/.zshrc
-ln -s $(pwd)/*.alias    ~/scripts
-ln -s $(pwd)/oh-my-zsh  ~/.oh-my-zsh
+ln -fs $(pwd)/*.alias   ~/.scripts/
 
 echo "Everything done :D"
