f49f66e525dfe2415972356c8c27f6b5e6b6add4
Felix Yadomi <dev.yadomi@gmail.com>
2015-11-02 10:40:02 +0100

    Update dotfiles - Mon Nov  2 10:40:02 CET 2015

--- install.sh
+++ install.sh
@@ -7,6 +7,5 @@ ln -s  $(pwd)/gitconfig    ~/.gitconfig
 ln -s  $(pwd)/zshrc        ~/.zshrc
 ln -fs $(pwd)/*.alias      ~/.scripts/
 ln -s  $(pwd)/oh-my-zsh/*  ~/.oh-my-zsh/
-ln -fs $(pwd)/atom/*       ~/.atom/
 
 echo "Everything done :D"
