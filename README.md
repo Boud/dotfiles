c832b2faecd691068cf95aad73deb8a5dbae01ce
Felix Yadomi <felix.yadomi@gmail.com>
2020-03-03 15:48:27 +0100

    Install with everything

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Yadomi's dotfiles
+# .dotfiles
 
 ```
 git clone git@github.com:yadomi/dotfiles.git .dotfiles
-cd .dotfiles
-./install.sh
+./.dotfiles/install.sh
 ```
--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 ROOT=$(readlink -f $(dirname "$0"))
 
-# git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-# ln -s $ROOT/.zprezto/modules/prompt/functions/prompt_yadomi_setup .zprezto/modules/prompt/functions/
+ln -s $ROOT/.zprezto/modules/prompt/functions/prompt_yadomi_setup .zprezto/modules/prompt/functions/
 ln -s $ROOT/.gitconfig ~/
-# ln -s $ROOT/.zpreztorc ~/
-# ln -s $ROOT/.zshrc ~/
+ln -s $ROOT/.zpreztorc ~/
+ln -s $ROOT/.zshrc ~/
